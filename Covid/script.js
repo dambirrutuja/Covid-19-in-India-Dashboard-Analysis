0cd2b1e684678987f78edbfaec33b059e4281672
dambirrutuja <dambirrutuja@gmail.com>
2026-01-28 17:16:49 -0500

    Add files via upload

---
+++ Covid/script.js
@@ -0,0 +1,357 @@
+// Tableau embed codes with placeholders for the unique IDs
+const tableauEmbeds = {
+    'Death in overall India': `
+        <div class='tableauPlaceholder' id='viz1727619241467' style='position: relative'>
+            <noscript>
+                <a href='#'>
+                    <img alt='Total Death ' src='https://public.tableau.com/static/images/Co/Covid-19inIndiaDashboardAnalysis_17262991203250/MapTotalDeathbyState/1_rss.png' style='border: none' />
+                </a>
+            </noscript>
+            <object class='tableauViz' style='display:none;'>
+                <param name='host_url' value='https%3A%2F%2Fpublic.tableau.com%2F' /> 
+                <param name='embed_code_version' value='3' /> 
+                <param name='site_root' value='' />
+                <param name='name' value='Covid-19inIndiaDashboardAnalysis_17262991203250&#47;MapTotalDeathbyState' />
+                <param name='tabs' value='no' />
+                <param name='toolbar' value='yes' />
+                <param name='static_image' value='https://public.tableau.com/static/images/Co/Covid-19inIndiaDashboardAnalysis_17262991203250/MapTotalDeathbyState/1.png' /> 
+                <param name='animate_transition' value='yes' />
+                <param name='display_static_image' value='yes' />
+                <param name='display_spinner' value='yes' />
+                <param name='display_overlay' value='yes' />
+                <param name='display_count' value='yes' />
+                <param name='language' value='en-US' />
+                <param name='filter' value='publish=yes' />
+            </object>
+        </div>`,
+
+    'Covid cases by state': `
+
+  <div class='tableauPlaceholder' id='viz1726353466827' style='position: relative'>
+    <noscript>
+        <a href='#'>
+            <img alt='Covid Cases by State' src='https://public.tableau.com/static/images/Co/Covid-19inIndiaDashboardAnalysis_17262991203250/CovidCasesbyState/1_rss.png' style='border: none' />
+        </a>
+    </noscript>
+    <object class='tableauViz' style='display:none;'>
+        <param name='host_url' value='https%3A%2F%2Fpublic.tableau.com%2F' />
+        <param name='embed_code_version' value='3' />
+        <param name='site_root' value='' />
+        <param name='name' value='Covid-19inIndiaDashboardAnalysis_17262991203250/CovidCasesbyState' />
+        <param name='tabs' value='no' />
+        <param name='toolbar' value='yes' />
+        <param name='static_image' value='https://public.tableau.com/static/images/Co/Covid-19inIndiaDashboardAnalysis_17262991203250/CovidCasesbyState/1.png' />
+        <param name='animate_transition' value='yes' />
+        <param name='display_static_image' value='yes' />
+        <param name='display_spinner' value='yes' />
+        <param name='display_overlay' value='yes' />
+        <param name='display_count' value='yes' />
+        <param name='language' value='en-US' />
+        <param name='filter' value='publish=yes' />
+    </object>
+</div>
+       
+       `,
+    'First and second dose administration':`
+
+   <div class='tableauPlaceholder' id='viz1726408402969' style='position: relative'>
+    <noscript>
+        <a href='#'>
+            <img alt='First vs Second dose Administration' src='https://public.tableau.com/static/images/Co/Covid-19inIndiaDashboardAnalysis_17262991203250/FirstvsSeconddoseAdmistration/1_rss.png' style='border: none' />
+        </a>
+    </noscript>
+    <object class='tableauViz' style='display:none;'>
+        <param name='host_url' value='https%3A%2F%2Fpublic.tableau.com%2F' /> 
+        <param name='embed_code_version' value='3' /> 
+        <param name='site_root' value='' />
+        <param name='name' value='Covid-19inIndiaDashboardAnalysis_17262991203250/FirstvsSeconddoseAdmistration' />
+        <param name='tabs' value='no' />
+        <param name='toolbar' value='yes' />
+        <param name='static_image' value='https://public.tableau.com/static/images/Co/Covid-19inIndiaDashboardAnalysis_17262991203250/FirstvsSeconddoseAdmistration/1.png' />
+        <param name='animate_transition' value='yes' />
+        <param name='display_static_image' value='yes' />
+        <param name='display_spinner' value='yes' />
+        <param name='display_overlay' value='yes' />
+        <param name='display_count' value='yes' />
+        <param name='language' value='en-US' />
+        <param name='filter' value='publish=yes' />
+    </object>
+</div> 
+
+    `,
+
+    'Doses administration by vaccine':`
+
+        <div class='tableauPlaceholder' id='viz1726342078004' style='position: relative'>
+    <noscript>
+        <a href='#'>
+            <img alt='Doses Administered by Vaccine' src='https://public.tableau.com/static/images/Co/Covid-19inIndiaDashboardAnalysis_17262991203250/DosesAdministeredbyVaccine/1_rss.png' style='border: none' />
+        </a>
+    </noscript>
+    <object class='tableauViz' style='display:none;'>
+        <param name='host_url' value='https%3A%2F%2Fpublic.tableau.com%2F' /> 
+        <param name='embed_code_version' value='3' /> 
+        <param name='site_root' value='' />
+        <param name='name' value='Covid-19inIndiaDashboardAnalysis_17262991203250/DosesAdministeredbyVaccine' />
+        <param name='tabs' value='no' />
+        <param name='toolbar' value='yes' />
+        <param name='static_image' value='https://public.tableau.com/static/images/Co/Covid-19inIndiaDashboardAnalysis_17262991203250/DosesAdministeredbyVaccine/1.png' />
+        <param name='animate_transition' value='yes' />
+        <param name='display_static_image' value='yes' />
+        <param name='display_spinner' value='yes' />
+        <param name='display_overlay' value='yes' />
+        <param name='display_count' value='yes' />
+        <param name='language' value='en-US' />
+        <param name='filter' value='publish=yes' />
+    </object>
+</div>
+    `,
+    'Age group':  `
+    
+    <div class='tableauPlaceholder' id='viz1727619143039' style='position: relative'>
+    <noscript>
+        <a href='#'>
+            <img alt='Age Group Details' src='https://public.tableau.com/static/images/Co/Covid-19inIndiaDashboardAnalysis_17262991203250/AgeGroupDetails/1_rss.png' style='border: none' />
+        </a>
+    </noscript>
+    <object class='tableauViz' style='display:none;'>
+        <param name='host_url' value='https%3A%2F%2Fpublic.tableau.com%2F' />
+        <param name='embed_code_version' value='3' />
+        <param name='site_root' value='' />
+        <param name='name' value='Covid-19inIndiaDashboardAnalysis_17262991203250/AgeGroupDetails' />
+        <param name='tabs' value='no' />
+        <param name='toolbar' value='yes' />
+        <param name='static_image' value='https://public.tableau.com/static/images/Co/Covid-19inIndiaDashboardAnalysis_17262991203250/AgeGroupDetails/1.png' />
+        <param name='animate_transition' value='yes' />
+        <param name='display_static_image' value='yes' />
+        <param name='display_spinner' value='yes' />
+        <param name='display_overlay' value='yes' />
+        <param name='display_count' value='yes' />
+        <param name='language' value='en-US' />
+        <param name='filter' value='publish=yes' />
+    </object>
+</div>
+    `,
+
+    'Gender':   `
+
+   <div class='tableauPlaceholder' id='viz1727614517180' style='position: relative'>
+    <noscript>
+        <a href='#'>
+            <img alt='Gender' src='https://public.tableau.com/static/images/Co/Covid-19inIndiaDashboardAnalysis_17262991203250/Gender/1_rss.png' style='border: none' />
+        </a>
+    </noscript>
+    <object class='tableauViz' style='display:none;'>
+        <param name='host_url' value='https%3A%2F%2Fpublic.tableau.com%2F' />
+        <param name='embed_code_version' value='3' />
+        <param name='site_root' value='' />
+        <param name='name' value='Covid-19inIndiaDashboardAnalysis_17262991203250/Gender' />
+        <param name='tabs' value='no' />
+        <param name='toolbar' value='yes' />
+        <param name='static_image' value='https://public.tableau.com/static/images/Co/Covid-19inIndiaDashboardAnalysis_17262991203250/Gender/1.png' />
+        <param name='animate_transition' value='yes' />
+        <param name='display_static_image' value='yes' />
+        <param name='display_spinner' value='yes' />
+        <param name='display_overlay' value='yes' />
+        <param name='display_count' value='yes' />
+        <param name='language' value='en-US' />
+        <param name='filter' value='publish=yes' />
+    </object>
+</div>
+    `,
+    'ICMR testing labs in each state': `
+    
+    <div class='tableauPlaceholder' id='viz1726342466521' style='position: relative'>
+    <noscript>
+        <a href='#'>
+            <img alt='ICMR Testing Labs in Each State' src='https://public.tableau.com/static/images/Co/Covid-19inIndiaDashboardAnalysis_17262991203250/ICMRTestingLabsinEachState/1_rss.png' style='border: none' />
+        </a>
+    </noscript>
+    <object class='tableauViz' style='display:none;'>
+        <param name='host_url' value='https%3A%2F%2Fpublic.tableau.com%2F' />
+        <param name='embed_code_version' value='3' />
+        <param name='site_root' value='' />
+        <param name='name' value='Covid-19inIndiaDashboardAnalysis_17262991203250/ICMRTestingLabsinEachState' />
+        <param name='tabs' value='no' />
+        <param name='toolbar' value='yes' />
+        <param name='static_image' value='https://public.tableau.com/static/images/Co/Covid-19inIndiaDashboardAnalysis_17262991203250/ICMRTestingLabsinEachState/1.png' />
+        <param name='animate_transition' value='yes' />
+        <param name='display_static_image' value='yes' />
+        <param name='display_spinner' value='yes' />
+        <param name='display_overlay' value='yes' />
+        <param name='display_count' value='yes' />
+        <param name='language' value='en-US' />
+        <param name='filter' value='publish=yes' />
+    </object>
+</div>
+    `,
+
+    'Statewise testing details':`
+
+  <div class='tableauPlaceholder' id='viz1726353855565' style='position: relative'>
+    <noscript>
+        <a href='#'>
+            <img alt='Statewise Testing Details' src='https://public.tableau.com/static/images/Co/Covid-19inIndiaDashboardAnalysis_17262991203250/StatewiseTestingDetails/1_rss.png' style='border: none' />
+        </a>
+    </noscript>
+    <object class='tableauViz' style='display:none;'>
+        <param name='host_url' value='https%3A%2F%2Fpublic.tableau.com%2F' />
+        <param name='embed_code_version' value='3' />
+        <param name='site_root' value='' />
+        <param name='name' value='Covid-19inIndiaDashboardAnalysis_17262991203250/StatewiseTestingDetails' />
+        <param name='tabs' value='no' />
+        <param name='toolbar' value='yes' />
+        <param name='static_image' value='https://public.tableau.com/static/images/Co/Covid-19inIndiaDashboardAnalysis_17262991203250/StatewiseTestingDetails/1.png' />
+        <param name='animate_transition' value='yes' />
+        <param name='display_static_image' value='yes' />
+        <param name='display_spinner' value='yes' />
+        <param name='display_overlay' value='yes' />
+        <param name='display_count' value='yes' />
+        <param name='language' value='en-US' />
+        <param name='filter' value='publish=yes' />
+    </object>
+</div>
+
+    `,
+
+    'No of Beds':`
+
+  <div class='tableauPlaceholder' id='viz1728331667114' style='position: relative'>
+    <noscript>
+        <a href='#'>
+            <img alt='Statewise Testing Details' src='https://public.tableau.com/static/images/Co/Covid-19inIndiaDashboardAnalysis_17262991203250/NoofBeds/1_rss.png' style='border: none' />
+        </a>
+    </noscript>
+    <object class='tableauViz' style='display:none;'>
+        <param name='host_url' value='https%3A%2F%2Fpublic.tableau.com%2F' />
+        <param name='embed_code_version' value='3' />
+        <param name='site_root' value='' />
+        <param name='name' value='Covid-19inIndiaDashboardAnalysis_17262991203250/NoofBeds' />
+        <param name='tabs' value='no' />
+        <param name='toolbar' value='yes' />
+        <param name='static_image' value='https://public.tableau.com/static/images/Co/Covid-19inIndiaDashboardAnalysis_17262991203250/NoofBeds/1.png' />
+        <param name='animate_transition' value='yes' />
+        <param name='display_static_image' value='yes' />
+        <param name='display_spinner' value='yes' />
+        <param name='display_overlay' value='yes' />
+        <param name='display_count' value='yes' />
+        <param name='language' value='en-US' />
+        <param name='filter' value='publish=yes' />
+    </object>
+</div>
+
+    `,
+
+'Statewise Gender Analysis':`
+
+  <div class='tableauPlaceholder' id='viz1728331952215' style='position: relative'>
+    <noscript>
+        <a href='#'>
+            <img alt='Statewise Testing Details' src='https://public.tableau.com/static/images/Co/Covid-19inIndiaDashboardAnalysis_17262991203250/StatewiseGenderAnalysis/1_rss.png' style='border: none' />
+        </a>
+    </noscript>
+    <object class='tableauViz' style='display:none;'>
+        <param name='host_url' value='https%3A%2F%2Fpublic.tableau.com%2F' />
+        <param name='embed_code_version' value='3' />
+        <param name='site_root' value='' />
+        <param name='name' value='Covid-19inIndiaDashboardAnalysis_17262991203250/StatewiseGenderAnalysis' />
+        <param name='tabs' value='no' />
+        <param name='toolbar' value='yes' />
+        <param name='static_image' value='https://public.tableau.com/static/images/Co/Covid-19inIndiaDashboardAnalysis_17262991203250/StatewiseGenderAnalysis/1.png' />
+        <param name='animate_transition' value='yes' />
+        <param name='display_static_image' value='yes' />
+        <param name='display_spinner' value='yes' />
+        <param name='display_overlay' value='yes' />
+        <param name='display_count' value='yes' />
+        <param name='language' value='en-US' />
+        <param name='filter' value='publish=yes' />
+    </object>
+</div>
+
+`
+
+};
+
+function handleSelection() {
+    
+        const dropdown = document.getElementById('inputField'); 
+        const selectedValue = dropdown.value;
+        console.log(selectedValue)
+        // Update the embed container with the selected Tableau code (without the script)
+        if (tableauEmbeds[selectedValue]) {
+        document.getElementById('tableauEmbed').innerHTML = tableauEmbeds[selectedValue];
+        
+        // Determine which div ID to use based on the selected value
+        let vizId = '';
+        switch (selectedValue) {
+            case 'Death in overall India':
+                vizId = 'viz1727619241467';
+                break;
+                
+                case 'Covid cases by state':
+                    vizId = 'viz1726353466827';
+                    break;
+                    
+                case 'First and second dose administration':
+                    vizId = 'viz1726408402969';
+                    break;
+                        
+                case 'Doses administration by vaccine':
+                    vizId = 'viz1726342078004';
+                    break;
+                    
+                case 'Age group':
+                    vizId = 'viz1727619143039';
+                    break;
+
+                case 'Gender':
+                    vizId = 'viz1727614517180';
+                    break;
+                
+                case 'ICMR testing labs in each state':
+                    vizId = 'viz1726342466521';
+                    break;
+                    
+                case 'Statewise testing details':
+                    vizId = 'viz1726353855565';
+                    break;
+
+                case 'No of Beds':
+                    vizId = 'viz1728331667114';
+                    break;
+
+                case 'Statewise Gender Analysis':
+                    vizId = 'viz1728331952215';
+                    break;
+
+
+            
+        }
+        
+        // Execute the custom Tableau resizing script after HTML is inserted
+        if (vizId) {
+            var divElement = document.getElementById(vizId);
+            if (divElement) {
+                var vizElement = divElement.getElementsByTagName('object')[0];
+                vizElement.style.width = '65rem';
+                vizElement.style.height = '33rem';
+                vizElement.style.position ='relative';
+                divElement.style.top = '2.5cm';
+                
+                
+                // Inject the Tableau API script manually
+                var scriptElement = document.createElement('script');
+                scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';
+                vizElement.parentNode.insertBefore(scriptElement, vizElement);
+            }
+        }
+        
+        // Shift the input field and show the script in the center
+        document.getElementById('DashBoardPage').classList.add('shifted');
+        document.getElementById('title-text').style.display = 'none';
+        document.getElementById('covidbg').style.display = 'none';
+        setTimeout(() => {
+            this.selectedIndex = 0;  // Reset to the first option (placeholder)
+        }, 100);
+    }
+}
